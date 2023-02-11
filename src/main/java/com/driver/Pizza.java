8dd465c6f52ce81b66016caf11515657911362b4
mahesh-7101 <86470981+mahesh-7101@users.noreply.github.com>
2023-02-11 12:44:26 +0530

    first commit

--- src/main/java/com/driver/Pizza.java
+++ src/main/java/com/driver/Pizza.java
@@ -5,10 +5,28 @@ public class Pizza {
     private int price;
     private Boolean isVeg;
     private String bill;
+    private int toppings;
+    boolean isExtraCheeseAdded;
+    boolean isExtraToppingAdded;
+    boolean isBillCreated;
+
+    boolean isTakeAway;
+
+
+
 
     public Pizza(Boolean isVeg){
         this.isVeg = isVeg;
         // your code goes here
+        if(isVeg){
+            this.price=300;
+            this.toppings=70;
+        }
+        else{
+            this.price =400;
+            this.toppings=120;
+        }
+        this.bill="Base Price of the Pizza:"+this.price+"\n";
     }
 
     public int getPrice(){
@@ -17,18 +35,41 @@ public class Pizza {
 
     public void addExtraCheese(){
         // your code goes here
+        if(!isExtraCheeseAdded){
+            this.price +=80;
+            this.isExtraCheeseAdded=true;
+        }
     }
 
     public void addExtraToppings(){
         // your code goes here
+        if(!isExtraToppingAdded){
+            this.price +=this.toppings;
+            this.isExtraToppingAdded=true;
+        }
     }
 
     public void addTakeaway(){
         // your code goes here
+        if(!isTakeAway){
+            this.price +=20;
+            isTakeAway =true;
+        }
     }
 
     public String getBill(){
         // your code goes here
-        return this.bill;
+        if(!isBillCreated) {
+            if (isExtraCheeseAdded)
+                this.bill += "Extra Cheese Added: 80" + "\n";
+            if (isExtraToppingAdded)
+                this.bill += "Extra Toppings Added:" + this.toppings + "\n";
+            if (isTakeAway)
+                this.bill += "Paperbag Added: 20" + "\n";
+            this.bill += "Total Price:" + this.price;
+            this.isBillCreated=true;
+            return this.bill;
+        }
+        return "";
     }
 }
